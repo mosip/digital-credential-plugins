c0dfd6ed49e95ad5cf322d67c492534e75642431
Challarao <challarao@beehyv.com>
2024-06-20 16:36:01 +0530

    [INJICERT-186] mock identity system call for getting details Signed-off-by: Challarao <challarao@beehyv.com>

--- mock-identity-certify-plugin/src/main/java/io.mosip.certify.mock.integration/service/MockVCIssuancePlugin.java
+++ mock-identity-certify-plugin/src/main/java/io.mosip.certify.mock.integration/service/MockVCIssuancePlugin.java
@@ -4,32 +4,41 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.security.GeneralSecurityException;
+import java.security.Key;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
+import javax.crypto.Cipher;
+
+import io.mosip.certify.api.dto.VCRequestDto;
+import io.mosip.certify.api.dto.VCResult;
 import io.mosip.certify.api.exception.VCIExchangeException;
+import io.mosip.certify.api.spi.VCIssuancePlugin;
 import io.mosip.certify.api.util.ErrorConstants;
+import io.mosip.certify.core.dto.ParsedAccessToken;
+import io.mosip.certify.core.exception.CertifyException;
+import io.mosip.esignet.core.dto.OIDCTransaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cache.CacheManager;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import foundation.identity.jsonld.ConfigurableDocumentLoader;
 import foundation.identity.jsonld.JsonLDException;
 import foundation.identity.jsonld.JsonLDObject;
 import info.weboftrust.ldsignatures.LdProof;
 import info.weboftrust.ldsignatures.canonicalizer.URDNA2015Canonicalizer;
-import io.mosip.certify.api.dto.VCRequestDto;
-import io.mosip.certify.api.dto.VCResult;
-import io.mosip.certify.api.spi.VCIssuancePlugin;
+import io.mosip.kernel.core.keymanager.spi.KeyStore;
 import io.mosip.kernel.core.util.CryptoUtil;
+import io.mosip.kernel.keymanagerservice.constant.KeymanagerConstant;
+import io.mosip.kernel.keymanagerservice.entity.KeyAlias;
+import io.mosip.kernel.keymanagerservice.helper.KeymanagerDBHelper;
 import io.mosip.kernel.signature.dto.JWTSignatureRequestDto;
 import io.mosip.kernel.signature.dto.JWTSignatureResponseDto;
 import io.mosip.kernel.signature.service.SignatureService;
@@ -39,24 +48,59 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 @Slf4j
 public class MockVCIssuancePlugin implements VCIssuancePlugin {
+
+	private static final String AES_CIPHER_FAILED = "aes_cipher_failed";
+	private static final String NO_UNIQUE_ALIAS = "no_unique_alias";
+	private static final String USERINFO_CACHE = "userinfo";
+
 	@Autowired
 	private SignatureService signatureService;
+
+	@Autowired
+	private ParsedAccessToken parsedAccessToken;
+
+	@Autowired
+	private CacheManager cacheManager;
+
+	@Autowired
+	private KeyStore keyStore;
+
+	@Autowired
+	private KeymanagerDBHelper dbHelper;
 
 	private ConfigurableDocumentLoader confDocumentLoader = null;
 
 	@Value("${mosip.certify.mock.vciplugin.verification-method}")
 	private String verificationMethod;
 
+	@Value("${mosip.certify.mock.authenticator.get-identity-url}")
+	private String getIdentityUrl;
+
+	@Value("${mosip.certify.cache.security.secretkey.reference-id}")
+	private String cacheSecretKeyRefId;
+
+	@Value("${mosip.certify.cache.security.algorithm-name}")
+	private String aesECBTransformation;
+
+	@Value("${mosip.certify.cache.secure.individual-id}")
+	private boolean secureIndividualId;
+
+	@Value("${mosip.certify.cache.store.individual-id}")
+	private boolean storeIndividualId;
+
+	@Value("#{${mosip.certify.mock.vciplugin.vc-credential-contexts:{'https://www.w3.org/2018/credentials/v1','https://schema.org/'}}}")
+	private List<String> vcCredentialContexts;
+
 	public static final String UTC_DATETIME_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
 
-	public static final String OIDC_SERVICE_APP_ID = "CERTIFY_SERVICE";
+	public static final String CERTIFY_SERVICE_APP_ID = "CERTIFY_SERVICE";
 
 	@Override
 	public VCResult<JsonLDObject> getVerifiableCredentialWithLinkedDataProof(VCRequestDto vcRequestDto, String holderId,
-			Map<String, Object> identityDetails) throws VCIExchangeException {
+																			 Map<String, Object> identityDetails) throws VCIExchangeException {
 		JsonLDObject vcJsonLdObject = null;
 		try {
-			VCResult vcResult = new VCResult();
+			VCResult<JsonLDObject> vcResult = new VCResult<>();
 			vcJsonLdObject = buildDummyJsonLDWithLDProof(holderId);
 			vcResult.setCredential(vcJsonLdObject);
 			vcResult.setFormat("ldp_vc");
@@ -69,15 +113,17 @@ public class MockVCIssuancePlugin implements VCIssuancePlugin {
 
 	private JsonLDObject buildDummyJsonLDWithLDProof(String holderId)
 			throws IOException, GeneralSecurityException, JsonLDException, URISyntaxException {
-		Map<String, Object> formattedMap = new HashMap<>();
-		formattedMap.put("id", holderId);
-		formattedMap.put("name", "John Doe");
-		formattedMap.put("email", "john.doe@mail.com");
-		formattedMap.put("gender", "Male");
+		OIDCTransaction transaction = getUserInfoTransaction(parsedAccessToken.getAccessTokenHash());
+		Map<String, Object> formattedMap = null;
+		try{
+			formattedMap = getIndividualData(transaction);
+		} catch(Exception e) {
+			log.error("Unable to get KYC exchange data from MOCK", e);
+		}
 
 		Map<String, Object> verCredJsonObject = new HashMap<>();
-		verCredJsonObject.put("@context", Arrays.asList("https://www.w3.org/2018/credentials/v1", "https://schema.org/"));
-		verCredJsonObject.put("type", Arrays.asList("VerifiableCredential", "Person"));
+		verCredJsonObject.put("@context", vcCredentialContexts);
+		verCredJsonObject.put("type", Arrays.asList("VerifiableCredential", "MOSIPVerifiableCredential"));
 		verCredJsonObject.put("id", "urn:uuid:3978344f-8596-4c3a-a978-8fcaba3903c5");
 		verCredJsonObject.put("issuer", "did:example:123456789");
 		verCredJsonObject.put("issuanceDate", getUTCDateTime());
@@ -101,7 +147,7 @@ public class MockVCIssuancePlugin implements VCIssuancePlugin {
 		String vcEncodedData = CryptoUtil.encodeToURLSafeBase64(vcSignBytes);
 
 		JWTSignatureRequestDto jwtSignatureRequestDto = new JWTSignatureRequestDto();
-		jwtSignatureRequestDto.setApplicationId(OIDC_SERVICE_APP_ID);
+		jwtSignatureRequestDto.setApplicationId(CERTIFY_SERVICE_APP_ID);
 		jwtSignatureRequestDto.setReferenceId("");
 		jwtSignatureRequestDto.setIncludePayload(false);
 		jwtSignatureRequestDto.setIncludeCertificate(true);
@@ -114,14 +160,81 @@ public class MockVCIssuancePlugin implements VCIssuancePlugin {
 		return vcJsonLdObject;
 	}
 
+	private Map<String, Object> getIndividualData(OIDCTransaction transaction){
+		String individualId = getIndividualId(transaction);
+		if (individualId!=null){
+			Map<String, Object> res = new RestTemplate().getForObject(
+					getIdentityUrl+"/"+individualId,
+					HashMap.class);
+			res = (Map<String, Object>)res.get("response");
+			Map<String, Object> ret = new HashMap<>();
+			ret.put("vcVer", "VC-V1");
+			ret.put("id", getIdentityUrl+"/"+individualId);
+			ret.put("UIN", individualId);
+			ret.put("name", res.get("name"));
+			ret.put("fullName", res.get("fullName"));
+			ret.put("gender", res.get("gender"));
+			ret.put("dateOfBirth", res.get("dateOfBirth"));
+			ret.put("email", res.get("email"));
+			ret.put("phone", res.get("phone"));
+			ret.put("addressLine1", res.get("streetAddress"));
+			ret.put("province", res.get("locality"));
+			ret.put("region", res.get("region"));
+			ret.put("postalCode", res.get("postalCode"));
+			ret.put("face", res.get("encodedPhoto"));
+			return ret;
+		} else {
+			return new HashMap<>();
+		}
+	}
+
+	protected String getIndividualId(OIDCTransaction transaction) {
+		if(!storeIndividualId)
+			return null;
+		return secureIndividualId ? decryptIndividualId(transaction.getIndividualId()) : transaction.getIndividualId();
+	}
+
+	private String decryptIndividualId(String encryptedIndividualId) {
+		try {
+			Cipher cipher = Cipher.getInstance(aesECBTransformation);
+			byte[] decodedBytes = Base64.getUrlDecoder().decode(encryptedIndividualId);
+			cipher.init(Cipher.DECRYPT_MODE, getSecretKeyFromHSM());
+			return new String(cipher.doFinal(decodedBytes, 0, decodedBytes.length));
+		} catch(Exception e) {
+			log.error("Error Cipher Operations of provided secret data.", e);
+			throw new CertifyException(AES_CIPHER_FAILED);
+		}
+	}
+
+	private Key getSecretKeyFromHSM() {
+		String keyAlias = getKeyAlias(CERTIFY_SERVICE_APP_ID, cacheSecretKeyRefId);
+		if (Objects.nonNull(keyAlias)) {
+			return keyStore.getSymmetricKey(keyAlias);
+		}
+		throw new CertifyException(NO_UNIQUE_ALIAS);
+	}
+
+	private String getKeyAlias(String keyAppId, String keyRefId) {
+		Map<String, List<KeyAlias>> keyAliasMap = dbHelper.getKeyAliases(keyAppId, keyRefId, LocalDateTime.now(ZoneOffset.UTC));
+		List<KeyAlias> currentKeyAliases = keyAliasMap.get(KeymanagerConstant.CURRENTKEYALIAS);
+		if (!currentKeyAliases.isEmpty() && currentKeyAliases.size() == 1) {
+			return currentKeyAliases.get(0).getAlias();
+		}
+		log.error("CurrentKeyAlias is not unique. KeyAlias count: {}", currentKeyAliases.size());
+		throw new CertifyException(NO_UNIQUE_ALIAS);
+	}
+
 	private static String getUTCDateTime() {
 		return ZonedDateTime.now(ZoneOffset.UTC).format(DateTimeFormatter.ofPattern(UTC_DATETIME_PATTERN));
 	}
 
 	@Override
 	public VCResult<String> getVerifiableCredential(VCRequestDto vcRequestDto, String holderId,
-			Map<String, Object> identityDetails) throws VCIExchangeException {
+													Map<String, Object> identityDetails) throws VCIExchangeException {
 		throw new VCIExchangeException(ErrorConstants.NOT_IMPLEMENTED);
 	}
 
+	public OIDCTransaction getUserInfoTransaction(String accessTokenHash) {
+		return cacheManager.getCache(USERINFO_CACHE).get(accessTokenHash, OIDCTransaction.class);
+	}
 }
