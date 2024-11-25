fae6e47ae8a150685b3b0303a64ffdb298f48a7c
Piyush7034 <piyushshukla2100@gmail.com>
2024-11-25 12:10:30 +0530

    Added postgres plugin in github workflows
    Signed-off-by: Piyush7034 <piyushshukla2100@gmail.com>

--- mock-postgres-dataprovider-plugin/src/main/java/io/mosip/certify/mockpostgresdataprovider/integration/repository/RegistrationReceiptDataRepositoryImpl.java
+++ mock-postgres-dataprovider-plugin/src/main/java/io/mosip/certify/mockpostgresdataprovider/integration/repository/RegistrationReceiptDataRepositoryImpl.java
@@ -3,23 +3,19 @@ package io.mosip.certify.mockpostgresdataprovider.integration.repository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.Query;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Repository;
 
-@Repository(value = "mockDataRepository")
-public class MockDataRepositoryImpl implements MockDataRepository {
+
+@Repository
+@ConditionalOnProperty(value = "mosip.certify.postgres.credential.name", havingValue = "RegistrationReceiptData")
+public class RegistrationReceiptDataRepositoryImpl implements MockDataRepository {
     @PersistenceContext
     private EntityManager entityManager;
 
-    @Value("${mosip.certify.postgres.data-provider.table-name}")
-    private String tableName;
-
-    @Value("${mosip.certify.postgres.data-provider.table-identifier}")
-    private String tableIdentifier;
-
     @Override
     public Object[] getIdentityDataFromIndividualId(String id) {
-        String queryString = "select * from " + tableName  + " where "  + tableIdentifier + "=:id";
+        String queryString = "select * from registration_receipt_data where registration_id=:id";
         Query query = entityManager.createNativeQuery(queryString);
         query.setParameter("id", id);
         return (Object[]) query.getSingleResult();
